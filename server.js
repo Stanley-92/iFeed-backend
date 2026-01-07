2ac95c6ceaccb39487f7dc7ad09e4f3518af8bbd
Stanley-92 <leystan405@gmail.com>
2026-01-07 13:04:29 +0700

    Update

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ app.post('/check-verified', async (req, res) => {
 })
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
